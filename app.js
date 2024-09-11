04763cdb5903b1028780e77a6fd8bc543f35abd3
HandharJunino <hjunino@gmail.com>
2024-09-11 20:01:21 +0100

    images, links and info

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const images = document.querySelectorAll('img');
-
-    for (const image of images) {
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 100
-            image.height = 100
-    })
-    }
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    const lastPostedElement = document.getElementById('last-posted');
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString('en-GB'); 
+    lastPostedElement.textContent = `Last Posted: ${formattedDate}`;
 });
